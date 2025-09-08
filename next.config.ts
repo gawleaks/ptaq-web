b59877d649ada8c984789ba8a8725b01e6f6f1ad
Szczepan Gawlik <cyber@gawliknet.eu>
2025-09-08 19:42:35 +0200

    removed export mode from next.js build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
   // GitHub Pages configuration
-  basePath: process.env.NODE_ENV === "production" ? "/ptaq-web" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/ptaq-web" : "",
+  // basePath: process.env.NODE_ENV === "production" ? "/ptaq-web" : "",
+  // assetPrefix: process.env.NODE_ENV === "production" ? "/ptaq-web" : "",
 };
 
 export default nextConfig;
